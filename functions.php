7a17ff39e561f3f2feeff0b2029a1f77f812cc8b
Geoffrey <geokh1992@gmail.com>
2016-05-16 18:42:00 +1000

    Added header and extra css

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 //
 // Recommended way to include parent theme styles.
 //  (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
-//  
+//
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -11,9 +11,7 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
 }
-//
-// Your code goes below
-//
+
 /**
  * Register widget area.
  *
@@ -29,23 +27,11 @@ function susanne_german_tutoring_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
-	register_sidebar( array(
-		'name' => 'Footer Sidebar 2',
-		'id' => 'footer-sidebar-2',
-		'description' => 'Appears in the footer area',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
-	register_sidebar( array(
-		'name' => 'Footer Sidebar 3',
-		'id' => 'footer-sidebar-3',
-		'description' => 'Appears in the footer area',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
 }
 add_action( 'widgets_init', 'susanne_german_tutoring_widgets_init' );
+
+function register_secondary_menu() {
+    register_nav_menu( 'secondary-menu', __( 'Secondary Menu' ) );
+}
+
+add_action( 'init', 'register_secondary_menu' );
